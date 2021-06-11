446eb7f7f5d59adf089865d9946533cc4f4a4529
IyobedZekarias <50155965+IyobedZekarias@users.noreply.github.com>
2021-06-11 11:47:24 -0500

    finished product

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ import Decentragram from '../abis/Decentragram.json'
 import Navbar from './Navbar'
 import Main from './Main'
 
+//Connect to ipfs for image upload
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+
 
 class App extends Component {
 
@@ -23,6 +27,7 @@ class App extends Component {
     }
   }
 
+  //make sure permissions are granted to a wallet before anything is able to be done on the page
   componentDidMount = async() => {
     this.getPermission()
   }
@@ -42,6 +47,7 @@ class App extends Component {
     
   }
 
+  //load the web3 and set it to be ethereum enabled on teh page
   async loadWeb3() {
     if(window.ethereum) {
       window.web3 = new Web3(window.ethereum)
@@ -56,7 +62,9 @@ class App extends Component {
   }
 
 
+  //get everything off of the blockchain
   async loadBlockchainData() {
+    //get the first account that the user wants to use
     const web3 = window.web3
     const accounts = await web3.eth.getAccounts()
     this.setState({
@@ -67,17 +75,81 @@ class App extends Component {
     const netID = await web3.eth.net.getId()
     const networkData = Decentragram.networks[netID]
     if(networkData){
+      //set decentragram as the contract in state
       const decentragram = web3.eth.Contract(Decentragram.abi, networkData.address)
-      this.setState({ decentragram})
+      this.setState({ decentragram })
 
-      const imagesCount = await decentragram.methods.imagesCount().call()
-      this.setState({ imagesCount})
+      //get the image count from the smart contract
+      const imagesCount = await decentragram.methods.imageCount().call()
+      this.setState({ imagesCount })
 
+      //loop through the images and get their hashes from the smart contract 
+      for(let i = 0; i < parseInt(imagesCount._hex); i++){
+        const image = await decentragram.methods.images(i).call()
+        this.setState({
+          images: [...this.state.images, image]
+        })
+      }
+
+      //order the images
+      this.setState({
+        images: this.state.images.sort((a,b) => b.tipAmount - a.tipAmount)
+      })
       this.setState({loading: false})
     } else {
       window.alert('Decentragram contract has not been deployed to detected network ')
     }
     
+  }
+
+  //take the uploaded image and put it in readable format for ipfs
+  captureFile = event => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) })
+      console.log('buffer', this.state.buffer)
+    }
+    
+  }
+
+  //send the image to ipfs 
+  //and take the returned hash and upload it to the smart contract
+  uploadImage = description => {
+    console.log("Submitting file to ipfs...")
+    
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('ipfs result', result )
+      if (error){
+        console.log(error)
+        return 
+      }
+
+
+      //upload the result to the smart contract
+      this.setState({ loading: true })
+      this.state.decentragram.methods
+        .uploadImage(result[0].hash, description)
+        .send({ from: this.state.account })
+        .on('transactionHash', hash => {
+          this.setState({ loading: false})
+      })
+    })
+  }
+
+  //make it so that images are able to be tipped .1 ETH
+  tipImageOwner = (id) => {
+    this.setState({ loading: true })
+    let value = window.web3.utils.toWei('.1', 'Ether')
+    this.state.decentragram.methods
+      .tipImageOwner(id)
+      .send({ from: this.state.account, value: value  })
+      .on('transactionHash', hash => {
+        this.setState({ loading: false })
+      })
   }
 
   render() {
@@ -86,9 +158,13 @@ class App extends Component {
         <Navbar account={this.state.account} />
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : (this.state.permission ? <Main
-            // Code...
-            /> : <p style={{marginTop: '20%', textAlign: 'center'}}>{this.state.message}</p>)
+          : (this.state.permission ? 
+              <Main 
+                captureFile={this.captureFile} 
+                uploadImage={this.uploadImage}
+                images={this.state.images}
+                tipImageOwner={this.tipImageOwner}/> 
+              : <p style={{marginTop: '20%', textAlign: 'center'}}>{this.state.message}</p>)
           }
       </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -27,12 +27,50 @@ class Main extends Component {
                         placeholder="Image description..."
                         required />
                   </div>
-                <button type="submit" class="btn btn-primary btn-block btn-lg">Upload!</button>
+                <button type="submit" className="btn btn-primary btn-block btn-lg">Upload!</button>
               </form>
 
               <p>&nbsp;</p>
                 
-                {/* Code ... */}
+              {/* this.props.images is an array of hashes that are saved on the smart contract */}
+              { this.props.images.map((image, key) => {
+                return(
+                  <div className="card mb-4" key={key} >
+                    <div className="card-header">
+                      <img
+                        className='mr-2'
+                        width='30'
+                        height='30'
+                        src={`data:image/png;base64,${new Identicon(image.author, 30).toString()}`}
+                      />
+                      <small className="text-muted">{image.author}</small>
+                    </div>
+                    <ul id="imageList" className="list-group list-group-flush">
+                      <li className="list-group-item">
+                        {/* Get the image off of ipfs using the hash received from the smart contract */}
+                        <p className="text-center"><img src={`https://ipfs.infura.io/ipfs/${image.hash}`} style={{ maxWidth: '420px'}}/></p>
+                        <p>{image.description}</p>
+                      </li>
+                      <li key={key} className="list-group-item py-2">
+                        <small className="float-left mt-1 text-muted">
+                          TIPS: {window.web3.utils.fromWei(image.tipAmount.toString(), 'Ether')} ETH
+                        </small>
+                        <button
+                          className="btn btn-link btn-sm float-right pt-0"
+                          name={image.id}
+                          onClick={(event) => {
+                            //let tipAmount = window.web3.utils.toWei('0.1', 'Ether')
+                            console.log(event.target.name)
+                            this.props.tipImageOwner(event.target.name)
+                          }}
+                        >
+                          TIP 0.1 ETH
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                )
+              })}
 
             </div>
           </main>
